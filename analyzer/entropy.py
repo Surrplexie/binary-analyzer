39f28a187fa6c07376fad6fc1081c8780ec4f852
Surrplexie <surrplexie@protonmail.com>
2026-03-26 03:29:26 -0500

    Updatte

--- analyzer/entropy.py
+++ analyzer/entropy.py
@@ -1,22 +1,24 @@
 import math
 
-def calculate_entropy(data):
-    import math
+def calculate_entropy(file_path):
+    with open(file_path, "rb") as f:
+        data = f.read()
 
     if not data:
         return 0
 
-    entropy = 0
     byte_counts = [0] * 256
 
     for byte in data:
         byte_counts[byte] += 1
 
+    entropy = 0
+    data_len = len(data)
+
     for count in byte_counts:
         if count == 0:
             continue
-        p = count / len(data)
+        p = count / data_len
         entropy -= p * math.log2(p)
 
     return entropy
-
